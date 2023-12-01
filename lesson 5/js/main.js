b56307de60f9d363c51744350b75eaac64d421c4
renatsharipov <renatibdev@mail.ru>
2023-12-01 22:51:08 +0500

    Исправлен вывод данных

--- lesson 5/js/main.js
+++ lesson 5/js/main.js
@@ -33,37 +33,37 @@ function createContent(item, dataType) {
 	switch (dataType) {
 		case 'posts':
 			return `
+			  <div class="userId">User ID: ${item.userId}</div>
         <div class="id">ID: ${item.id}</div>
-        <div class="userId">User ID: ${item.userId}</div>
         <div class="title">Title: ${item.title}</div>
         <div class="body">Body: ${item.body}</div>
       `
 		case 'comments':
 			return `
+				<div class="postId">Post ID: ${item.postId}</div>
         <div class="id">ID: ${item.id}</div>
-        <div class="postId">Post ID: ${item.postId}</div>
         <div class="name">Name: ${item.name}</div>
         <div class="email">Email: ${item.email}</div>
         <div class="body">Body: ${item.body}</div>
       `
 		case 'albums':
 			return `
+				<div class="userId">User ID: ${item.userId}</div>
         <div class="id">ID: ${item.id}</div>
-        <div class="userId">User ID: ${item.userId}</div>
         <div class="title">Title: ${item.title}</div>
       `
 		case 'photos':
 			return `
+				<div class="albumId">Album ID: ${item.albumId}</div>
         <div class="id">ID: ${item.id}</div>
-        <div class="albumId">Album ID: ${item.albumId}</div>
         <div class="title">Title: ${item.title}</div>
         <div class="url">URL: ${item.url}</div>
         <div class="thumbnailUrl">Thumbnail URL: ${item.thumbnailUrl}</div>
       `
 		case 'todos':
 			return `
+				<div class="userId">User ID: ${item.userId}</div>
         <div class="id">ID: ${item.id}</div>
-        <div class="userId">User ID: ${item.userId}</div>
         <div class="title">Title: ${item.title}</div>
         <div class="completed">Completed: ${item.completed}</div>
       `
@@ -74,11 +74,27 @@ function createContent(item, dataType) {
         <div class="username">Username: ${item.username}</div>
         <div class="email">Email: ${item.email}</div>
         <div class="address">
+					<p>address</p>
           <div class="street">Street: ${item.address.street}</div>
           <div class="suite">Suite: ${item.address.suite}</div>
           <div class="city">City: ${item.address.city}</div>
           <div class="zipcode">Zipcode: ${item.address.zipcode}</div>
+					<div class="geo">
+					<p>geo</p>
+					<div class="lat">lat: ${item.address.geo.lat}</div>
+					<div class="lng">lng: ${item.address.geo.lng}</div>
+				</div>
         </div>
+				<div class="phone">Phone:${item.phone}</div>
+				<div class="website">Website:${item.website}</div>
+				<div class="company">
+					<p>Company</p>
+					<div class="name">Name:${item.company.name}</div>
+					<div class="catchPhrase">catchPhrase:${item.company.catchPhrase}</div>
+					<div class="bs">bs:${item.company.bs}</div>
+				</div>
+
+				
       `
 		default:
 			return 'Данных пока нет. Нажми на кнопку для получения интересуемых данных с сервера :)'
